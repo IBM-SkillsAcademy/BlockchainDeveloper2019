b5f0f0786d7cb6ba6b332e956119cc00d441ab9d
blockchain <blockchain@blockchain-machine>
2019-06-04 21:59:38 +0000

    inital commit for project files Vehicle Manufacture life cycle

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { VehicleContract } from './VehicleContract';
+export { VehicleContract } from './VehicleContract';
+
+export const contracts: any[] = [ VehicleContract ];
---
+++ src/vehicle.ts
@@ -0,0 +1,7 @@
+export class Vehicle {
+    public docType?: string;
+    public color: string;
+    public make: string;
+    public model: string;
+    public owner: string;
+}
