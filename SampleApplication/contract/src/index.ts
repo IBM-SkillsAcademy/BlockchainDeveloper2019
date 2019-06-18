08590136aa94f4cca75364d80c675ae31a7b0751
Dean Zaka Hidayat <hdeanzak@id.ibm.com>
2019-06-18 20:36:20 +0700

    remove out of place SampleApplication folder

--- SampleApplication/contract/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { VehicleContract } from './VehicleContract';
-export { VehicleContract } from './VehicleContract';
-
-export const contracts: any[] = [ VehicleContract ];
--- SampleApplication/contract/src/vehicle.ts
+++
@@ -1,7 +0,0 @@
-export class Vehicle {
-    public docType?: string;
-    public color: string;
-    public make: string;
-    public model: string;
-    public owner: string;
-}
