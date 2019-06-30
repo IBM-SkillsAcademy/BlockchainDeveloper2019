e5da6b5b5d9b4ff0511aa32f3ae3788de41fbc51
Norhan <nkhaled280@gmail.com>
2019-07-01 00:35:39 +0200

    policy  functions without swagger

--- SampleApplication/application/org1/api/v1/vehicle/controller.js
+++ SampleApplication/application/org1/api/v1/vehicle/controller.js
@@ -9,6 +9,52 @@ const walletPath = path.join(process.cwd(), 'wallet');
 const wallet = new FileSystemWallet(walletPath);
 console.log(`Wallet path: ${walletPath}`);
 
+exports.placeOrder = async (req, res, next) => {
+  try {
+    const enrollmentID = req.headers['enrollment-id'];
+    // get connection profile
+    const ccp = await utils.getCCP();
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(enrollmentID);
+    if (!userExists) {
+      return res.status(401).send({
+        message: `An identity for the user ${enrollmentID} does not exist in the wallet`
+      });
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: enrollmentID, discovery: { enabled: false } });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('vehicle-manufacture');
+
+    // Submit the specified transaction.
+    // placeOrder transaction - requires 5 argument, ex: ('placeOrder', 'order12', 'Tom' ', Honda', 'Accord', 'Black')
+    await contract.submitTransaction(
+      'placeOrder',
+      req.body.orderId,
+      req.body.owner,
+      req.body.make,
+      req.body.model,
+      req.body.color);
+
+    // Disconnect from the gateway.
+    await gateway.disconnect();
+    return res.send({
+      message: `Order with ID ${req.body.orderId} has been created`,
+      details: req.body
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
 exports.createVehicle = async (req, res, next) => {
   try {
     const enrollmentID = req.headers['enrollment-id'];
@@ -186,3 +232,142 @@ exports.getPrice = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getPolicy = async (req, res, next) => {
+  try {
+    const enrollmentID = req.headers['enrollment-id'];
+
+    // get connection profile
+    const ccp = await utils.getCCP();
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(enrollmentID);
+    if (!userExists) {
+      return res.status(401).send({
+        message: `An identity for the user ${enrollmentID} does not exist in the wallet`
+      });
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: enrollmentID, discovery: { enabled: false } });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('vehicle-manufacture');
+
+    // Evaluate the specified transaction.
+    let result, rawResult;
+
+    if (req.query.id) {
+    // if policy id specified getPolicy transaction - requires 1 argument, ex: ('getPolicy', 'policy123')
+      result = await contract.evaluateTransaction('getPolicy', req.query.id);
+      rawResult = result.toString();
+    } else {
+    throw new Error('Not Policy found');
+    }
+    const json = JSON.parse(rawResult);
+    const obj = JSON.parse(json);
+    return res.send({
+      result: obj
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+exports.getPolicies = async (req, res, next) => {
+  try {
+    const enrollmentID = req.headers['enrollment-id'];
+
+    // get connection profile
+    const ccp = await utils.getCCP();
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(enrollmentID);
+    if (!userExists) {
+      return res.status(401).send({
+        message: `An identity for the user ${enrollmentID} does not exist in the wallet`
+      });
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: enrollmentID, discovery: { enabled: false } });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('vehicle-manufacture');
+
+    // Evaluate the specified transaction.
+    let result, rawResult;
+
+      // getPolicies transaction - requires no arguments, ex: ('getPolicies')
+      result = await contract.evaluateTransaction('getPolicies');
+      rawResult = result.toString();
+    
+    const json = JSON.parse(rawResult);
+    const obj = JSON.parse(json);
+    return res.send({
+      result: obj
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+exports.requestPolicy = async (req, res, next) => {
+  try {
+    const enrollmentID = req.headers['enrollment-id'];
+    // get connection profile
+    const ccp = await utils.getCCP();
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(enrollmentID);
+    if (!userExists) {
+      return res.status(401).send({
+        message: `An identity for the user ${enrollmentID} does not exist in the wallet`
+      });
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: enrollmentID, discovery: { enabled: false } });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('vehicle-manufacture');
+
+    // Submit the specified transaction.
+    // requestPolicy transaction - requires 8 argument, ex: ('requestPolicy', 'vin12', 'insurer12', 'holder12', 'THIRD_PARTY', 12122019 , 31122020)
+    await contract.submitTransaction(
+      'requestPolicy',
+      req.body.id,
+      req.body.vin,
+      req.body.insurerId,
+      req.body.holderId,
+      req.body.policyType,
+      req.body.startDate, 
+      req.body.endDate);
+
+    // Disconnect from the gateway.
+    await gateway.disconnect();
+    return res.send({
+      message: `Vehicle with ID ${req.body.vehicleID} has been created`,
+      details: req.body
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+
--- SampleApplication/application/org1/api/v1/vehicle/index.js
+++ SampleApplication/application/org1/api/v1/vehicle/index.js
@@ -7,10 +7,11 @@ const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-
+router.post('/order' , controller.placeOrder);
 router.post('/', controller.createVehicle);
 router.get('/', controller.getVehicle);
 router.get('/price', controller.getPrice);
 router.post('/price', controller.updatePrice);
-
+router.post('/policy' , controller.getPolicy);
+router.get('/policy' , controller.getPolicies);
 module.exports = router;
