4c76efe01062b2689bd48c3350f237a7e2561a47
Norhan Khaled <nkhaled280@gmail.com>
2019-07-04 14:49:08 +0200

    3 more functions in Org1 application

--- SampleApplication/application/org1/api/v1/vehicle/controller.js
+++ SampleApplication/application/org1/api/v1/vehicle/controller.js
@@ -497,3 +497,125 @@ exports.requestVIN = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getHistoryForVehicle = async (req, res, next) => {
+  try {
+    const enrollmentID = req.headers['enrollment-id'];
+
+    // get connection profile
+    const ccp = await utils.getCCP();
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(enrollmentID);
+    if (!userExists) {
+      return res.status(401).send({
+        message: `An identity for the user ${enrollmentID} does not exist in the wallet`
+      });
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: enrollmentID });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('vehicle-manufacture');
+
+    // Evaluate the specified transaction.
+    const result = await contract.evaluateTransaction('getHistoryForVehicle', req.query.id);
+    const rawResult = result.toString();
+
+    const obj = JSON.parse(rawResult);
+    return res.send({
+      result: obj
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+exports.getHistoryForOrder = async (req, res, next) => {
+  try {
+    const enrollmentID = req.headers['enrollment-id'];
+
+    // get connection profile
+    const ccp = await utils.getCCP();
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(enrollmentID);
+    if (!userExists) {
+      return res.status(401).send({
+        message: `An identity for the user ${enrollmentID} does not exist in the wallet`
+      });
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: enrollmentID });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('vehicle-manufacture');
+
+    // Evaluate the specified transaction.
+    const result = await contract.evaluateTransaction('getHistoryForOrder', req.query.id);
+    const rawResult = result.toString();
+
+    const obj = JSON.parse(rawResult);
+    return res.send({
+      result: obj
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+exports.getOrdersByStatusPaginated = async (req, res, next) => {
+  try {
+    const enrollmentID = req.headers['enrollment-id'];
+
+    // get connection profile
+    const ccp = await utils.getCCP();
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(enrollmentID);
+    if (!userExists) {
+      return res.status(401).send({
+        message: `An identity for the user ${enrollmentID} does not exist in the wallet`
+      });
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: enrollmentID });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('vehicle-manufacture');
+
+    // Evaluate the specified transaction. 
+    if(!req.query.bookmark) {
+      req.query.bookmark = "";
+    }
+    const result = await contract.evaluateTransaction('getOrdersByStatusPaginated', req.query.orderStatus , req.query.pageSize , req.query.bookmark);
+
+    const rawResult = result.toString();
+
+    const obj = JSON.parse(rawResult);
+    return res.send({
+      result: obj
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
