82448df54e4159ff542bb602bde4cf8f6a31ed44
Keanu Nurherbyanto <knurherb@id.ibm.com>
2019-07-22 20:37:46 +0700

    added ex02 comments in main branch

--- SampleApplication/contract/src/utils/vehicleContext.ts
+++ SampleApplication/contract/src/utils/vehicleContext.ts
@@ -1,12 +1,20 @@
 import { Context, Contract } from 'fabric-contract-api';
 import { Order } from '../assets/order';
-import { Policy } from '../assets/policy';
 import { Vehicle } from '../assets/vehicle';
 import { OrderList } from '../lists/orderList';
-import { PolicyList } from '../lists/policyList';
 import { PriceList } from '../lists/priceList';
 import { VehicleList } from '../lists/vehicleList';
 import { Price } from './../assets/price';
+/**
+ * *** Exercise 02 > Part 4 > Step 6 ***
+ */
+
+/*
+Import the policy and policy list class to the vehicle context to allow the
+smart contract application to recognize and interact with the policy asset.
+*/
+import { Policy } from '../assets/policy';
+import { PolicyList } from '../lists/policyList';
 
 /* Custom Context that extend chain code Context class
  Used to define
@@ -15,24 +23,31 @@ https://hyperledger-fabric.readthedocs.io/en/latest/developapps/transactionconte
 */
 export class VehicleContext extends Context {
     private orderList: OrderList<Order>;
-    private policyList: PolicyList<Policy>;
     private priceList: PriceList<Price>;
     private vehicleList: VehicleList<Vehicle>;
+    // Define policyList as list of policy objects
+    private policyList: PolicyList<Policy>;
     constructor() {
         super();
 
         this.orderList = new OrderList(this, [Order]);
-        this.policyList = new PolicyList(this, [Policy]);
         this.vehicleList = new VehicleList(this, [Vehicle]);
         this.priceList = new PriceList(this, [Price]);
+        // Create policy list upon context class construction
+        this.policyList = new PolicyList(this, [Policy]);
     }
 
     public getOrderList(): OrderList<Order> {
         return this.orderList;
     }
+    /*
+    Helper function to allow the vehicle context to
+    retrieve policy list class and interact with its functions
+    */
     public getPolicyList(): PolicyList<Policy> {
         return this.policyList;
     }
+
     public getVehicleList(): VehicleList<Vehicle> {
         return this.vehicleList;
     }
