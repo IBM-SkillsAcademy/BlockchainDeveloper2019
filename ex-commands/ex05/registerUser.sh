ef40b767e2caab5fc13f5f5aff464b69fd76c7c8
Dean Zaka Hidayat <hdeanzak@id.ibm.com>
2019-07-24 21:51:56 +0700

    modify ca name to domain

--- ex-commands/ex05/registerUser.sh
+++ ex-commands/ex05/registerUser.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Register and Enrolling User " echo $1 
+echo "Register and Enrolling User $1" 
 
 CA_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ca.org1.example.com`
-echo "IP of CA ( ca.org1.example.com ) server "
+echo "IP of CA ( ca.org1.example.com ) server ${CA_IP}"
 count=`cat /etc/hosts | sed -n "/ca.org1.example.com/p" | wc -l`
 echo "-------START -----Adding CA server IP to hosts file"
 if [ "${count}" -gt 0 ]
@@ -10,7 +10,7 @@ then
  echo "CA exists in hosts file Replace"
    sed -i "/ca.org1.example.com/c\ ${CA_IP}  ca.org1.example.com" /etc/hosts
 else
-    echo " CA doesn't exist in Hosts"
+   echo "CA doesn't exist in Hosts"
    sed -i "1i ${CA_IP} ca.org1.example.com" /etc/hosts    
 fi
 echo "------- END  -----Adding CA server IP to hosts file"
