3e2a0858b26761a29e2ab37974f38fe3a04f77f2
Keanu Nurherbyanto <knurherb@id.ibm.com>
2019-07-29 06:36:27 +0200

    edited enrollUser shell script

--- ex-commands/ex05/enrollUser.sh
+++ ex-commands/ex05/enrollUser.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Register and Enrolling User $1 $2" 
+echo "Register and Enrolling User $1 $2 $3 $4" 
 
 CA_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ca.$2.example.com`
 echo "IP of CA ( ca.$2.example.com ) server ${CA_IP}"
@@ -25,12 +25,9 @@ chmod +777 -R $HOME/fabric-ca/client/
 
 fabric-ca-client enroll -u https://admin:adminpw@ca.$2.example.com:7054  --tls.certfiles ca.$2.example.com-cert.pem
 
-ATTRS=role=$3:ecert
-OUTPUT=$(fabric-ca-client register --id.type client --id.name $1 --id.affiliation $2.department1 --id.attrs ${ATTRS} --id.attrs 'hf.Revoker=true,admin=true:ecert' --tls.certfiles ca.$2.example.com-cert.pem | tail -1)
+ATTRS=role=$4:ecert
+OUTPUT=$(fabric-ca-client register --id.type client --id.name $1 --id.affiliation $2.$3 --id.attrs ${ATTRS} --tls.certfiles ca.$2.example.com-cert.pem | tail -1)
 PASSWORD=$(echo $OUTPUT | awk -F" " '{print $2}')
 
 fabric-ca-client enroll -u https://$1:${PASSWORD}@ca.$2.example.com:7054  --tls.certfiles ca.$2.example.com-cert.pem
 cp -r $HOME/fabric-ca/client/msp/signcerts $HOME/fabric-ca/client/msp/admincerts
-
-docker exec cli mkdir /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$2.example.com/users/$1@$2.example.com
-docker cp $HOME/fabric-ca/client/msp cli:/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/user6@org1.example.com/msp
