b57b354eed366be3f69e26bde4ef7b91882494d8
Norhan Khaled <nkhaled280@gmail.com>
2019-08-09 02:09:07 +0200

    updating EX07 start command

--- ex-commands/ex07/startEx07.sh
+++ ex-commands/ex07/startEx07.sh
@@ -2,18 +2,22 @@
 ROOT=${PWD}
 echo "Update Contract files with Completed EX06"
 
-#cp -R ex07Artifacts/contract/. ../../SampleApplication/contract/
+cd /home/blockchain/Desktop/BlockchainDeveloper2019/ex-commands/ex06/solution
+./applySolution.sh
 
 
 echo "################## START NETWORK ########################"
-cd ../../Vehicle-Network/
+cd /home/blockchain/BlockchainDeveloper2019/Vehicle-Network
+
+
+./byfn.sh down
 ./byfn.sh up -V 1.9.2
 
 echo "Update Contract with Ex07 Artifacts "
 cd $ROOT
-#cd ex03Artifacts
+
 pwd
-cp -R ex07Artifacts/application/insurer/. ../../SampleApplication/application/insurer/
-cp -R ex07Artifacts/application/manufacturer/. ../../SampleApplication/manufacturer/
+cp -R ex07Artifacts/application/insurer/api/v1/vehicles/controller.js ../../SampleApplication/application/insurer/api/v1/vehicles/controller.js
+cp -R ex07Artifacts/application/manufacturer/api/v1/vehicles/controller.js ../../SampleApplication/application/manufacturer/api/v1/vehicles/controller.js
 
 
