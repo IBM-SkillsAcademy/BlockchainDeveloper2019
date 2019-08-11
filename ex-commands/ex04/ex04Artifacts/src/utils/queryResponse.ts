668f7de4059aa0dd241d71eb1c2893b3b4481a20
Labib Farag <Labib.Farag.saad@gmail.com>
2019-08-11 13:59:35 +0000

    add ex04 artifacts

---
+++ ex-commands/ex04/ex04Artifacts/src/utils/queryResponse.ts
@@ -0,0 +1,11 @@
+import { Object as ContractObject, Property } from 'fabric-contract-api';
+
+// Query Response as container for Query result
+export class QueryResponse {
+    @Property()
+    public timestamp: number;
+    @Property()
+    public txId: string;
+    public key: string;
+    public record: string;
+}
