5d2b3876536ca549fba14aeccf84cbcee86d7aae
Labib Farag <Labib.Farag.saad@gmail.com>
2019-08-12 21:50:21 +0000

    update ex03 artifacts

--- ex-commands/ex03/startEx03.sh
+++ ex-commands/ex03/startEx03.sh
@@ -14,11 +14,7 @@ echo "Update Contract with Ex03 Artifacts "
 cd $ROOT
 #cd ex03Artifacts
 pwd
-cp -R ex03Artifacts/assets/. ../../SampleApplication/contract/src/assets/
-cp -R ex03Artifacts/utils/. ../../SampleApplication/contract/src/utils/
-cp -R ex03Artifacts/lists/. ../../SampleApplication/contract/src/lists/
-cp -R ex03Artifacts/ledger-api/. ../../SampleApplication/contract/src/ledger-api/
-cp  ex03Artifacts/VehicleContract.ts ../../SampleApplication/contract/src/contracts/VehicleContract.ts
+cp -R ex03Artifacts/src/. ../../SampleApplication/contract/src
 cp  ex03Artifacts/package.json ../../SampleApplication/contract/src/contracts/package.json
 
 
