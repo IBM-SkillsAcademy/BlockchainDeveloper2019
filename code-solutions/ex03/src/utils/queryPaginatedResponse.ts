c349d25c07f9a530b97ce08119716c424dce4871
Labib Farag <Labib.Farag.saad@gmail.com>
2019-08-20 06:48:56 +0000

    add code solution ex03

---
+++ code-solutions/ex03/src/utils/queryPaginatedResponse.ts
@@ -0,0 +1,18 @@
+import { State } from '../ledger-api/state';
+// Query response as a container for query results with pagination option
+/*
+The container defines attributes for fetched record count and bookmark
+*/
+
+export class QueryPaginationResponse  <T extends State> {
+    public value: T[];
+    public fetched_records_count: number;
+    public bookmark: string;
+
+    constructor(fetched_records_count: number, bookmark: string) {
+        this.fetched_records_count = fetched_records_count;
+        this.bookmark = bookmark;
+
+    }
+
+}
