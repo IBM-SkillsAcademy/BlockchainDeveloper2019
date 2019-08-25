d4d157ac6b521569c2362134434d83c823ae49ae
Labib Farag <Labib.Farag.saad@gmail.com>
2019-08-25 08:11:31 +0000

    update upgrade command

--- Vehicle-Network/upgrade.sh
+++ Vehicle-Network/upgrade.sh
@@ -28,3 +28,19 @@ cli bash -c "peer chaincode upgrade -o orderer.example.com:7050 --tls \
 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
 -C mychannel -n vehicle-manufacture -l node -v $1 -c '{\"Args\":[]}' -P \"AND ('Org1MSP.member','Org2MSP.member', 'Org3MSP.member')\" \
 --collections-config \$GOPATH/src/github.com/chaincode/collections_config.json"
+
+sleep 10
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" \
+-e "CORE_PEER_ADDRESS=peer0.org2.example.com:9051" \
+-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
+-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+cli peer chaincode query -C mychannel -n vehicle-manufacture -c '{"Args":["queryVehicle","4567788:Prius"]}' >> logs.txt
+sleep 10
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" \
+-e "CORE_PEER_ADDRESS=peer0.org3.example.com:11051" \
+-e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp" \
+-e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
+cli peer chaincode query -C mychannel -n vehicle-manufacture -c '{"Args":["queryVehicle","4567788:Prius"]}' >> logs.txt
+
+echo "successfully upgraded smart contract"
