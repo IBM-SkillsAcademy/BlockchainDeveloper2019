5ad1af78ee1cc2ba928616447551febec404947a
Labib Farag <Labib.Farag.saad@gmail.com>
2019-08-25 08:12:37 +0000

    update ex03 and ex04 commands

---
+++ ex-commands/ex03/applySolution.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# copy solution file
+cp -R ../../code-solutions/ex03/src/. ../../SampleApplication/contract/src/
+cp    ../../code-solutions/ex03/package.json ../../SampleApplication/contract/
+
+
