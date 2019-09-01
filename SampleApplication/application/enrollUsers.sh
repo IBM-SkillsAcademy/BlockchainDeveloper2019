4446aa15caa8b7aa594f84aaa31150a841f6a38c
blockchain <nkhaled280@gmail.com>
2019-09-01 12:41:17 +0200

    ex08 commands update

--- SampleApplication/application/enrollUsers.sh
+++ SampleApplication/application/enrollUsers.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 echo " enroll Admin and users"
 
@@ -9,11 +8,11 @@ curl -X GET "http://localhost:6002/api/v1/auth/registrar/enroll" -H  "accept: */
 curl -X GET "http://localhost:6003/api/v1/auth/registrar/enroll" -H  "accept: */*"
 
 curl -X GET "http://localhost:6003/api/v1/auth/create-affiliation" -H  "accept: */*"
-curl -X POST "http://localhost:6001/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"user1\"}"
+curl -X POST "http://localhost:6001/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Manu-User\"}"
 
-curl -X POST "http://localhost:6002/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"user2\"}"
+curl -X POST "http://localhost:6002/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Regu-User\"}"
 
-curl -X POST "http://localhost:6003/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"user3\"}"
+curl -X POST "http://localhost:6003/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Insu-User\"}"
 
 echo "Admins and Users are enrolled in the 3 applications"
 
