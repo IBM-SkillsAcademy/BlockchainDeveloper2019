c3f9cb7a33b7974c31f06abbcc3524edaf828763
blockchain <nkhaled280@gmail.com>
2019-09-01 14:09:38 +0200

    update regulator swagger

--- ex-commands/ex08/populateData.sh
+++ ex-commands/ex08/populateData.sh
@@ -10,10 +10,10 @@ ls
 ./enrollUsers.sh
 ./populateOrders.sh
 
-curl -X POST "http://localhost:6001/api/v1/vehicles" -H  "accept: */*" -H  "enrollment-id: user1" -H  "Content-Type: application/json" -d "{\"orderID\":\"Order1\",\"manufacturer\":\"Honda\",\"model\":\"Accord\",\"color\":\"Black\",\"owner\":\"Tom\"}"
+curl -X POST "http://localhost:6001/api/v1/vehicles" -H  "accept: */*" -H  "enrollment-id: Manu-User" -H  "Content-Type: application/json" -d "{\"orderID\":\"Order1\",\"manufacturer\":\"Honda\",\"model\":\"Accord\",\"color\":\"Black\",\"owner\":\"Tom\"}"
 
 
-curl -X POST "http://localhost:6001/api/v1/vehicles/policies/request" -H  "accept: */*" -H  "enrollment-id: user1" -H  "Content-Type: application/json" -d "{\"id\":\"policy1\",\"vehicleNumber\":\"Order1:Accord\",\"insurerId\":\"insurer1\",\"holderId\":\"holder1\",\"policyType\":\"THIRD_PARTY\",\"startDate\":\"12122019\",\"endDate\":\"31122019\"}"
+curl -X POST "http://localhost:6001/api/v1/vehicles/policies/request" -H  "accept: */*" -H  "enrollment-id: Manu-User" -H  "Content-Type: application/json" -d "{\"id\":\"policy1\",\"vehicleNumber\":\"Order1:Accord\",\"insurerId\":\"insurer1\",\"holderId\":\"holder1\",\"policyType\":\"THIRD_PARTY\",\"startDate\":\"12122019\",\"endDate\":\"31122019\"}"
 
 
 ./stop.sh
