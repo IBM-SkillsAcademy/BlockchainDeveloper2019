e4b255d21f5ec379fec16dee937fb76988f94a93
Norhan Khaled <nkhaled280@gmail.com>
2019-09-05 20:38:23 +0200

    Update enrollUsers.sh

--- SampleApplication/application/enrollUsers.sh
+++ SampleApplication/application/enrollUsers.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 echo " enroll Admin and users"
+sleep 10
 
 curl -X GET "http://localhost:6001/api/v1/auth/registrar/enroll" -H  "accept: */*"
 
 curl -X GET "http://localhost:6002/api/v1/auth/registrar/enroll" -H  "accept: */*"
 
 curl -X GET "http://localhost:6003/api/v1/auth/registrar/enroll" -H  "accept: */*"
-
+sleep 5
 curl -X GET "http://localhost:6003/api/v1/auth/create-affiliation" -H  "accept: */*"
-curl -X POST "http://localhost:6001/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"user1\"}"
+sleep 5
+curl -X POST "http://localhost:6001/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Manu-User\"}"
 
-curl -X POST "http://localhost:6002/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"user2\"}"
+curl -X POST "http://localhost:6002/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Regu-User\"}"
 
-curl -X POST "http://localhost:6003/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"user3\"}"
+curl -X POST "http://localhost:6003/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Insu-User\"}"
 
 echo "Admins and Users are enrolled in the 3 applications"
 
