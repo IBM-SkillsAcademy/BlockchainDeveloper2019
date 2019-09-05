6e13aa511bab95bf85ae43ecba6a2b3cbf4e7008
Norhan Khaled <nkhaled280@gmail.com>
2019-09-05 20:37:33 +0200

    Update enrollUsers.sh

--- SampleApplication/application/enrollUsers.sh
+++ SampleApplication/application/enrollUsers.sh
@@ -7,8 +7,9 @@ curl -X GET "http://localhost:6001/api/v1/auth/registrar/enroll" -H  "accept: */
 curl -X GET "http://localhost:6002/api/v1/auth/registrar/enroll" -H  "accept: */*"
 
 curl -X GET "http://localhost:6003/api/v1/auth/registrar/enroll" -H  "accept: */*"
-
+sleep 5
 curl -X GET "http://localhost:6003/api/v1/auth/create-affiliation" -H  "accept: */*"
+sleep 5
 curl -X POST "http://localhost:6001/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Manu-User\"}"
 
 curl -X POST "http://localhost:6002/api/v1/auth/user/register-enroll" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"enrollmentID\":\"Regu-User\"}"
