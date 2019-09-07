975ed1d6d6a445f4ee048c8cbb70f738d7ca6b98
Labib Farag <Labib.Farag.saad@gmail.com>
2019-09-07 13:59:58 +0000

    update ex04 start command

--- ex-commands/ex04/startEx04.sh
+++ ex-commands/ex04/startEx04.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+count=`cat /etc/resolv.conf | sed -n "/options/p" | wc -l`
+if [ "${count}" -gt 0 ]
+then
+  echo blockchain | sudo -S sed -i "/options /c\ " /etc/resolv.conf
+fi
 
 # apply last exercise solution
 rm -rf ../../SampleApplication/contract/src
